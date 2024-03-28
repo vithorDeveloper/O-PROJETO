7e7b45a93a75c39e6fb56726286ebbf51cf6194e
vithorDeveloper <vitorsantosdev663@gmail.com>
2024-03-28 07:27:44 -0300

    add o comandos `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1>Marcando o "marco 0" aqui, e o primeiro dia do meu desafio, "um commit todo dia"</h1>
+  return (
+    <h1>
+      Marcando o "marco 0" aqui, e o primeiro dia do meu desafio, "um commit
+      todo dia"
+    </h1>
+  );
+};
+
+function ola() {
+  console.log("ola");
 }
 
-export default Home
+export default Home;
